7fa01d0d8118556529ff40f25029a40bfc4a7ca7
buzlukerdem <buzlukerdem@gmail.com>
2025-01-16 00:16:52 +0300

    singleton dp, practices

--- SingletonDesignPatternTest/Program.cs
+++ SingletonDesignPatternTest/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+// Singleton Design Pattern Practices
+#region 1.Practice
+var instance = MyClass.GetMyClassInstance();
+var instance2 = MyClass.GetMyClassInstance();
+var instance3 = MyClass.GetMyClassInstance();
+var instance4 = MyClass.GetMyClassInstance();
+
+
+class MyClass
+{
+    //new ile nesne üretimi engellendi.
+    private MyClass()
+    {
+        Console.WriteLine($"{nameof(MyClass)} nesnesi üretildi.");
+    }
+    //static global field
+    static MyClass _myClassInstance;
+    //static ctor
+    static MyClass()
+    {
+        _myClassInstance = new MyClass();
+    }
+    // static member 
+    public static MyClass GetMyClassInstance()
+    {
+        #region 1.Yöntem Kontrollü
+        if (_myClassInstance == null)
+            _myClassInstance = new MyClass();
+        return _myClassInstance;
+        #endregion
+        #region 2.Yöntem kontrolsüz.
+        return _myClassInstance;
+        #endregion
+
+    }
+}
+
+#endregion
+
